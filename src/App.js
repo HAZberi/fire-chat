8f62b6c616cd42c033e827cbec5ee40133694408
Hassaan Zuberi <mhazuberi@gmail.com>
2021-04-09 06:52:14 -0600

    firebase configuration object implemented

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import "firebase/auth";
 import "firebase/firestore"
 import './App.css';
 
-firebase.initializeApp({});
+firebase.initializeApp({
+  apiKey: "AIzaSyApOEHiPjIFKd27s4J-z0oRzMKAANFvjYU",
+  authDomain: "fire-chat-7274f.firebaseapp.com",
+  projectId: "fire-chat-7274f",
+  storageBucket: "fire-chat-7274f.appspot.com",
+  messagingSenderId: "668383367706",
+  appId: "1:668383367706:web:309d353d544bbddce077a3",
+  measurementId: "G-02WLXWX251"
+});
 
 const FireChat = () => {
   return (
