eb31d899f68283ba0df1afa9bf9091908363b4f8
Hassaan Zuberi <mhazuberi@gmail.com>
2021-04-09 08:11:02 -0600

    Google Sign In Implemented

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import React, { useState, useEffect } from "react";
 import firebase from "firebase/app";
+
 import "firebase/auth";
-import "firebase/firestore"
-import Button from "./Button"
-import './App.css';
+import "firebase/firestore";
+import Button from "./Button";
+import "./App.css";
 
 firebase.initializeApp({
   apiKey: "AIzaSyApOEHiPjIFKd27s4J-z0oRzMKAANFvjYU",
@@ -11,13 +13,17 @@ firebase.initializeApp({
   storageBucket: "fire-chat-7274f.appspot.com",
   messagingSenderId: "668383367706",
   appId: "1:668383367706:web:309d353d544bbddce077a3",
-  measurementId: "G-02WLXWX251"
+  measurementId: "G-02WLXWX251",
 });
 
+const fireChatAuth = firebase.auth();
+
 const FireChat = () => {
+  const [user, setUser] = useState(() => fireChatAuth.currentUser);
+  const [initialization, setInitialization] = useState(true);
   const signInWithGoogle = async () => {
     //fetch google auth provider
-    const googleAuth = new firebase.auth.GithubAuthProvider();
+    const googleAuth = new firebase.auth.GoogleAuthProvider();
     //set the default language
     firebase.auth().useDeviceLanguage();
     //start the sign in process
@@ -26,15 +32,28 @@ const FireChat = () => {
     } catch (err) {
       console.error(err.message);
     }
+  };
 
-  }
-  
+  useEffect(() => {
+    const subscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(false);
+      }
+      if (initialization) {
+        setInitialization(false);
+      }
+      //cleanup OR componentWillUnmount
+      return subscribe;
+    });
+  }, [initialization]);
 
   return (
     <div className="App">
       <Button onClick={signInWithGoogle}>Sign In With Google</Button>
     </div>
   );
-}
+};
 
 export default FireChat;
